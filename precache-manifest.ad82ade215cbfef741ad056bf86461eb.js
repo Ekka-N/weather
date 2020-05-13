240971f58dba8776a8f3ca4fb43b3fc6360d4e82
Ekaterina Nikishina <ekkin3@gmail.com>
2020-05-13 14:09:32 +0300

    Updates

---
+++ precache-manifest.ad82ade215cbfef741ad056bf86461eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89825982d82698d6dd7d8249cdfe7adf",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "96575da46bb20a6f017f",
+    "url": "/weather/static/css/main.0febc280.chunk.css"
+  },
+  {
+    "revision": "e7832730fbc64fdd1757",
+    "url": "/weather/static/js/2.bd9257c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.bd9257c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96575da46bb20a6f017f",
+    "url": "/weather/static/js/main.8034671e.chunk.js"
+  },
+  {
+    "revision": "08cf2222bde43000caed",
+    "url": "/weather/static/js/runtime-main.c045791e.js"
+  }
+]);
